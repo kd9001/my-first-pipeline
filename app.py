2625f9945552d31fde5c96941dfbc4cf9684d791
kd9001 <kd9001@yahoo.co.in>
2026-05-07 22:23:26 -0500

    added web server to app

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from my pipeline app!")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    print("Server running on port 8080")
+    server.serve_forever()
